410f233d2bc09a28c9b0f0ef6cda0f9274c11fd3
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-20 16:56:55 -0300

    img nova

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import "../Css/home.css"
 
+import imgFundoArvores from "../img/fundoInicialComImagem.png"
+
 const Home = () => {
   return (
     <div>
 
         <h4>Breath Natureal</h4>
+
+<img src={imgFundoArvores} alt="" className='imgArvore' />
+
     </div>
   )
 }
