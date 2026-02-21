70b401167fad710cec772d58102f7c386b96ac04
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-21 09:47:42 -0300

    teste

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
 
       <div className="buyPlant">
-        <img src={plantBuyNow} alt="" className='imgComprarImgPrincipal' />
+      <img src={plantBuyNow} alt="" className='imgComprarImgPrincipal' />
            <div className="movimetnacaoConteudoDentroDiv">
            <p>Trendy House Plant</p>
             <h2>Calathea Plant</h2>
