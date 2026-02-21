9b962c2248c19801db3d9b58b6635d4808ebcb76
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-21 11:58:11 -0300

    teste

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -54,6 +54,8 @@ const Home = () => {
         <div className="conteudo">
           <h5>For Small Decs Ai Plat</h5>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing</p>
+          <span>Rs. 599/-</span>
+          <button>Explore</button>
         </div>
       </div>
 
